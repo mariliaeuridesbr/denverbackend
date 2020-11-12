70ddec0bc7f6271aeb1f83fcbfece2cc5d975272
mariliaeuridesbr <67061731+mariliaeuridesbr@users.noreply.github.com>
2020-11-12 15:35:24 -0300

    Testing some implementations

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 const acl = require("express-acl");
 const {config,responseObject} = require("./config/acl");
+const { Client } = require('pg');
 
 require('express-async-errors');
 require('./database');
@@ -14,3 +15,20 @@ acl.config(config, responseObject);
 app.use(routes);
 
 app.listen(process.env.PORT || 3030);
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
