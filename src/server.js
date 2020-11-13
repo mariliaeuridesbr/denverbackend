9ad9a2181b35e4b397e067e5aadd0d426600173a
mariliaeuridesbr <67061731+mariliaeuridesbr@users.noreply.github.com>
2020-11-13 11:14:21 -0300

    Trying to implement Heroku Postgres

--- src/server.js
+++ src/server.js
@@ -15,3 +15,20 @@ acl.config(config, responseObject);
 app.use(routes);
 
 app.listen(process.env.PORT || 3030);
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+  
+  client.connect();
+  
+  client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
+});
