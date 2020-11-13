6c2ccb011b42b1d381c8b1874213b89530a1f785
mariliaeuridesbr <67061731+mariliaeuridesbr@users.noreply.github.com>
2020-11-13 11:40:27 -0300

    Trying to implement Heroku Postgres

--- src/config/database.js
+++ src/config/database.js
@@ -12,18 +12,18 @@ module.exports = {
         timestamps: true,
         underscored: true,
     }*/
-    "development": {
-      "username": process.env.DB_USER,
-      "password": process.env.DB_PASS,
-      "database": process.env.DB_DATABASE,
-      "host": process.env.DB_HOST,
-      "port": process.env.DB_PORT,
-      "dialect": "postgres",
-      "operatorsAliases": false
+    development: {
+      dialect: "postgres",
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      operatorsAliases: false
     },
-    "production": {
-      "use_env_variable": "DATABASE_URL",
-      "dialect": "postgres",
-      "operatorsAliases": false
+    production: {
+      dialect: "postgres",
+      use_env_variable: "DATABASE_URL",
+      operatorsAliases: false
     }
 }
