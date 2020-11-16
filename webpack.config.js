c7191650b8da6bc1e2d7550b44960dd1088f4adf
Bruno De Masi <brunodemasi1@gmail.com>
2020-11-16 20:23:16 +0000

    feat: hello

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './src/server.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'api.bundle.js',
+  },
+  target: 'node',
+  externals: [nodeExternals()]
+}
